fdd2c8a2177e54b67ad127a9b3469784109ec2c8
Kelley Gibson <kelley@thephuse.com>
2014-12-04 23:39:09 -0500

    Add smooth scroll.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
   var bodyOffset = $('body').offset().top;
   var headerHeight = $('header').height();
+  var viewportHeight = $(window).height();
+  console.log(viewportHeight/2);
+
 
   $(window).scroll(function(){
     if($('header').hasClass('scroll')){
@@ -12,5 +15,16 @@ $(document).ready(function(){
     if($('header').offset().top > $('header').height()){
       $('header').addClass('scroll');
     }
+  });
+
+  //nav click functions
+  $('header a').click(function(e){
+    var href = $(this).attr('href').substring(1);
+    var target = $('#'+href).offset().top;
+    console.log(headerHeight);
+    e.preventDefault();
+    $('a.active').removeClass('active');
+    $(this).addClass('active');
+    $('body').scrollTo(target-headerHeight, 1000);
   });
 });
